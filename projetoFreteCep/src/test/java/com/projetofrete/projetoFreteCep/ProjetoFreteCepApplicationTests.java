0baea9a3df6a1f9d42ca73a2d72ec670330ec60c
Dieg0Ch4ves <diegoaraujo200570@gmail.com>
2023-04-25 13:19:09 -0400

    frete-cep

---
+++ projetoFreteCep/src/test/java/com/projetofrete/projetoFreteCep/ProjetoFreteCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofrete.projetoFreteCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFreteCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
